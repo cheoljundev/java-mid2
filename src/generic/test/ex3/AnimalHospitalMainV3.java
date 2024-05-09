8b2001c0218829679ddded8182dd5be6207cecf0
cheoljundev <cheoljundev@gmail.com>
2024-05-09 16:33:21 +0900

    제네릭 타입 매개변수 제한

---
+++ src/generic/test/ex3/AnimalHospitalMainV3.java
@@ -0,0 +1,30 @@
+package generic.test.ex3;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalMainV3 {
+    public static void main(String[] args) {
+        AnimalHospitalV3<Dog> dogHospital = new AnimalHospitalV3<>();
+        AnimalHospitalV3<Cat> catHospital = new AnimalHospitalV3<>();
+        // AnimalHospitalV3<Integer> integerHospital = new AnimalHospitalV3<>(); // 불가능
+
+        Dog dog = new Dog("멍멍이1", 100);
+        Cat cat = new Cat("야옹이1", 300);
+
+        dogHospital.set(dog);
+        dogHospital.checkUp();
+
+        catHospital.set(cat);
+        catHospital.checkUp();
+
+        // 문제1: 개 병원에 고양이 전달
+        // dogHospital.set(cat); // 매개변수 체크 성공
+
+        // 문제2: 개 타입 반환
+        dogHospital.set(dog);
+        Dog biggerDog = dogHospital.bigger(new Dog("멍멍이2", 200));
+        System.out.println("biggerDog = " + biggerDog);
+    }
+
+}
