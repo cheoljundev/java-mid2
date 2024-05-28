a1b6bc395a03f5ac73a06033064e2012b6ba04b1
cheoljundev <cheoljundev@gmail.com>
2024-05-28 15:33:29 +0900

    iterable

---
+++ src/collection/map/test/iterable/JavaIterableMain.java
@@ -0,0 +1,45 @@
+package collection.map.test.iterable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class JavaIterableMain {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Iterator<Integer> iterator1 = list.iterator();
+
+        printAll(iterator1);
+
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        Iterator<Integer> iterator2 = set.iterator();
+        printAll(iterator2);
+
+        forEach(list);
+        forEach(set);
+
+
+    }
+
+    private static void forEach(Iterable<Integer> iterable) {
+        System.out.println("iterable = " + iterable.getClass());
+        for (Integer integer : iterable) {
+            System.out.println("integer = " + integer);
+        }
+    }
+
+    private static void printAll(Iterator<Integer> iterator) {
+        System.out.println("iterator = " + iterator.getClass());
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
