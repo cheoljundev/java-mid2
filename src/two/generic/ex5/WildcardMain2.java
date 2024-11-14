5973ad206961f5110cb7926c247f75ce2e7470f6
cheoljundev <cheoljundev@gmail.com>
2024-11-15 00:03:33 +0900

    와일드카드2

---
+++ src/two/generic/ex5/WildcardMain2.java
@@ -0,0 +1,26 @@
+package two.generic.ex5;
+
+import two.generic.animal.Animal;
+import two.generic.animal.Cat;
+import two.generic.animal.Dog;
+
+public class WildcardMain2 {
+    public static void main(String[] args) {
+        Box<Object> objBox = new Box<>();
+        Box<Animal> animalBox = new Box<>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+
+        writeBox(objBox);
+        writeBox(animalBox);
+        // writeBox(dogBox); // 하한이 Animal
+        // writeBox(catBox);
+
+        Animal animal = animalBox.get();
+        System.out.println("animal = " + animal);
+    }
+
+    static void writeBox(Box<? super Animal> box) {
+        box.set(new Dog("멍멍이", 20));
+    }
+}
