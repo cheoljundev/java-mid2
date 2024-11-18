bddcb83ea7c2012c58c8d2743a0518b4536fbdff
cheoljundev <cheoljundev@gmail.com>
2024-11-18 20:18:59 +0900

    리스트 추상화1 - 인터페이스 도입

---
+++ src/two/collection/list/MyLinkedList.java
@@ -0,0 +1,130 @@
+package two.collection.list;
+
+public class MyLinkedList<E> implements MyList<E> {
+    private Node<E> first;
+    private int size = 0;
+
+    @Override
+    public void add(E e) {
+        Node<E> newNode = new Node<>(e);
+        if (first == null) {
+            first = newNode;
+        } else {
+            Node<E> lastNode = getLastNode();
+            lastNode.next = newNode;
+        }
+        size++;
+    }
+
+    private Node<E> getLastNode() {
+        Node<E> x = first;
+        while (x.next != null) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    @Override
+    public void add(int index, E e) {
+        Node<E> newNode = new Node<>(e);
+        if (index == 0) {
+            newNode.next = first;
+            first = newNode;
+        } else {
+            Node<E> prevNode = getNode(index - 1);
+            newNode.next = prevNode.next;
+            prevNode.next = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public E set(int index, E e) {
+        Node<E> x = getNode(index);
+        E oldElement = x.item;
+        x.item = e;
+        return oldElement;
+    }
+
+    @Override
+    public E remove(int index) {
+        E target;
+        if (index == 0) {
+            target = first.item;
+            first = first.next;
+        } else {
+            Node<E> prevNode = getNode(index - 1);
+            Node<E> oldNode = prevNode.next;
+            prevNode.next = oldNode.next;
+            target = oldNode.item;
+            oldNode.item = null;
+            oldNode.next = null;
+        }
+        size--;
+        return target;
+    }
+
+    @Override
+    public E get(int index) {
+        Node<E> node = getNode(index);
+        return node.item;
+    }
+
+    private Node<E> getNode(int index) {
+        Node<E> x = first;
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    @Override
+    public int indexOf(E e) {
+        int index = 0;
+        for (Node<E> x = first; x != null; x = x.next) {
+            if (e.equals(x.item)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyLinkedListV1{" +
+                "first=" + first +
+                ", size=" + size +
+                '}';
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+
+        public Node(E item) {
+            this.item = item;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            Node x = this;
+            sb.append("[");
+            while (x != null) {
+                sb.append(x.item);
+                if (x.next != null) {
+                    sb.append("->");
+                }
+                x = x.next;
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+    }
+}
