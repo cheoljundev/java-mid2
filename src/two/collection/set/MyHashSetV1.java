2673e6826990bd726dab8f73d8ae1fd5bd3cf187
cheoljundev <cheoljundev@gmail.com>
2024-11-22 22:00:01 +0900

    직접 구현하는 Set1 - MyHashSetV1

---
+++ src/two/collection/set/MyHashSetV1.java
@@ -0,0 +1,72 @@
+package two.collection.set;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MyHashSetV1 {
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    LinkedList<Integer>[] buckets;
+    private int size = 0;
+    private int capacity = DEFAULT_INITIAL_CAPACITY;
+
+    public MyHashSetV1() {
+        this(DEFAULT_INITIAL_CAPACITY);
+    }
+
+    public MyHashSetV1(int capacity) {
+        this.capacity = capacity;
+        initBuckets();
+    }
+
+    private void initBuckets() {
+        buckets = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    public boolean add(int value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Integer> bucket = buckets[hashIndex];
+        if (bucket.contains(value)) {
+            return false;
+        }
+        bucket.add(value);
+        size++;
+        return true;
+    }
+
+    public boolean contains(int searchValue) {
+        int hashIndex = hashIndex(searchValue);
+        LinkedList<Integer> bucket = buckets[hashIndex];
+        return bucket.contains(searchValue);
+    }
+
+    public boolean remove(int value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Integer> bucket = buckets[hashIndex];
+        if (bucket.contains(value)) {
+            bucket.remove(Integer.valueOf(value));
+            size--;
+            return true;
+        }
+        return false;
+    }
+
+    public int hashIndex(int value) {
+        return value % capacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV1{" +
+                "buckets=" + Arrays.toString(buckets) +
+                ", size=" + size +
+                ", capacity=" + capacity +
+                '}';
+    }
+}
